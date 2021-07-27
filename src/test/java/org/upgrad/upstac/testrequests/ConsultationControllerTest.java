c0511b8f4cac0ecd4382a121f900495aa3cffe29
rushiPE89 <55731339+rushiPE89@users.noreply.github.com>
2021-07-27 09:26:56 +0530

    Unit test ConsultationControllerTest
    - ConsultationControllerTest : Added implementation for the unit test cases.

--- src/test/java/org/upgrad/upstac/testrequests/ConsultationControllerTest.java
+++ src/test/java/org/upgrad/upstac/testrequests/ConsultationControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.web.server.ResponseStatusException;
 import org.upgrad.upstac.testrequests.TestRequest;
 import org.upgrad.upstac.testrequests.consultation.ConsultationController;
 import org.upgrad.upstac.testrequests.consultation.CreateConsultationRequest;
+import org.upgrad.upstac.testrequests.consultation.DoctorSuggestion;
 import org.upgrad.upstac.testrequests.lab.TestStatus;
 import org.upgrad.upstac.testrequests.RequestStatus;
 import org.upgrad.upstac.testrequests.TestRequestQueryService;
@@ -39,18 +40,10 @@ class ConsultationControllerTest {
 
         TestRequest testRequest = getTestRequestByStatus(RequestStatus.LAB_TEST_COMPLETED);
 
-        //Implement this method
-
-        //Create another object of the TestRequest method and explicitly assign this object for Consultation using assignForConsultation() method
-        // from consultationController class. Pass the request id of testRequest object.
-
-        //Use assertThat() methods to perform the following two comparisons
-        //  1. the request ids of both the objects created should be same
-        //  2. the status of the second object should be equal to 'DIAGNOSIS_IN_PROCESS'
-        // make use of assertNotNull() method to make sure that the consultation value of second object is not null
-        // use getConsultation() method to get the lab result
-
-
+        TestRequest newTestRequest = consultationController.assignForConsultation(testRequest.getRequestId());
+        assertThat(testRequest.getRequestId(), equalTo(newTestRequest.getRequestId()));
+        assertThat(newTestRequest.getStatus(), equalTo(RequestStatus.DIAGNOSIS_IN_PROCESS));
+        assertNotNull(newTestRequest.getConsultation());
     }
 
     public TestRequest getTestRequestByStatus(RequestStatus status) {
@@ -62,17 +55,11 @@ class ConsultationControllerTest {
     public void calling_assignForConsultation_with_valid_test_request_id_should_throw_exception(){
 
         Long InvalidRequestId= -34L;
+        ResponseStatusException exception = assertThrows(ResponseStatusException.class,()->{
+            consultationController.assignForConsultation(InvalidRequestId);
+        });
 
-        //Implement this method
-
-
-        // Create an object of ResponseStatusException . Use assertThrows() method and pass assignForConsultation() method
-        // of consultationController with InvalidRequestId as Id
-
-
-        //Use assertThat() method to perform the following comparison
-        //  the exception message should be contain the string "Invalid ID"
-
+        assertThat(exception.getMessage(), containsString("Invalid ID"));
     }
 
     @Test
@@ -81,21 +68,12 @@ class ConsultationControllerTest {
 
         TestRequest testRequest = getTestRequestByStatus(RequestStatus.DIAGNOSIS_IN_PROCESS);
 
-        //Implement this method
-        //Create an object of CreateConsultationRequest and call getCreateConsultationRequest() to create the object. Pass the above created object as the parameter
+        CreateConsultationRequest createConsultationRequest = getCreateConsultationRequest(testRequest);
+        TestRequest updatedTestRequest =  consultationController.updateConsultation(testRequest.getRequestId(),createConsultationRequest);
 
-        //Create another object of the TestRequest method and explicitly update the status of this object
-        // to be 'COMPLETED'. Make use of updateConsultation() method from consultationController class
-        // (Pass the previously created two objects as parameters)
-        // (for the object of TestRequest class, pass its ID using getRequestId())
-
-        //Use assertThat() methods to perform the following three comparisons
-        //  1. the request ids of both the objects created should be same
-        //  2. the status of the second object should be equal to 'COMPLETED'
-        // 3. the suggestion of both the objects created should be same. Make use of getSuggestion() method to get the results.
-
-
-
+        assertThat(updatedTestRequest.getRequestId(),equalTo(testRequest.getRequestId()));
+        assertThat(updatedTestRequest.getStatus(),equalTo(RequestStatus.COMPLETED));
+        assertThat(updatedTestRequest.getConsultation().getSuggestion(),equalTo(createConsultationRequest.getSuggestion()));
     }
 
 
@@ -105,18 +83,11 @@ class ConsultationControllerTest {
 
         TestRequest testRequest = getTestRequestByStatus(RequestStatus.DIAGNOSIS_IN_PROCESS);
 
-        //Implement this method
-
-        //Create an object of CreateConsultationRequest and call getCreateConsultationRequest() to create the object. Pass the above created object as the parameter
-
-        // Create an object of ResponseStatusException . Use assertThrows() method and pass updateConsultation() method
-        // of consultationController with a negative long value as Id and the above created object as second parameter
-        //Refer to the TestRequestControllerTest to check how to use assertThrows() method
-
-
-        //Use assertThat() method to perform the following comparison
-        //  the exception message should be contain the string "Invalid ID"
-
+        CreateConsultationRequest createConsultationRequest = getCreateConsultationRequest(testRequest);
+        ResponseStatusException exception = assertThrows(ResponseStatusException.class,()->{
+            consultationController.updateConsultation(-1L,createConsultationRequest);
+        });
+        assertThat(exception.getMessage(), containsString("Invalid ID"));
     }
 
     @Test
@@ -125,28 +96,26 @@ class ConsultationControllerTest {
 
         TestRequest testRequest = getTestRequestByStatus(RequestStatus.DIAGNOSIS_IN_PROCESS);
 
-        //Implement this method
+        CreateConsultationRequest createConsultationRequest = getCreateConsultationRequest(testRequest);
+        createConsultationRequest.setSuggestion(null);
+        ResponseStatusException exception = assertThrows(ResponseStatusException.class,()->{
+            consultationController.updateConsultation(testRequest.getRequestId(),createConsultationRequest);
 
-        //Create an object of CreateConsultationRequest and call getCreateConsultationRequest() to create the object. Pass the above created object as the parameter
-        // Set the suggestion of the above created object to null.
-
-        // Create an object of ResponseStatusException . Use assertThrows() method and pass updateConsultation() method
-        // of consultationController with request Id of the testRequest object and the above created object as second parameter
-        //Refer to the TestRequestControllerTest to check how to use assertThrows() method
-
-
+        });
+        assertThat(exception.getMessage(), containsString("ConstraintViolationException"));
     }
 
     public CreateConsultationRequest getCreateConsultationRequest(TestRequest testRequest) {
 
-        //Create an object of CreateLabResult and set all the values
-        // if the lab result test status is Positive, set the doctor suggestion as "HOME_QUARANTINE" and comments accordingly
-        // else if the lab result status is Negative, set the doctor suggestion as "NO_ISSUES" and comments as "Ok"
-        // Return the object
+        CreateConsultationRequest createConRequest = new CreateConsultationRequest();
+        if(testRequest.getLabResult().getResult().equals(TestStatus.POSITIVE)){
+            createConRequest.setComments("Looks ok, take medicines at home");
+            createConRequest.setSuggestion(DoctorSuggestion.HOME_QUARANTINE);
+        } else{
+            createConRequest.setComments("Looks normal. No issues.");
+            createConRequest.setSuggestion(DoctorSuggestion.NO_ISSUES);
+        }
 
-
-        return null; // Replace this line with your code
-
+        return createConRequest;
     }
-
 }
